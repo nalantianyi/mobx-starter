0d347c5e32359eab7e426c7f58dc283614199982
nalantianyi <nalantianyi@icloud.com>
2017-09-08 16:45:51 +0800

    //

--- web/index.js
+++ web/index.js
@@ -1,55 +1,6 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import {observer} from 'mobx-react';
-import {observable, computed,autorun} from 'mobx';
+import Timer from './timer/index';
 
+ReactDOM.render(<Timer/>, document.getElementById('root'));
 
-class Todo {
-    id = Math.random();
-    @observable title = '';
-    @observable finished = false;
-    constructor(title){
-        this.title=title;
-    }
-}
-
-class TodoList {
-    @observable todos = [];
-
-    @computed
-    get unfinishedTodoCount() {
-        return this.todos.filter((todo) => !todo.finished).length;
-    }
-}
-
-const TodoView = observer(({todo}) => {
-    return <li>
-        <input type="checkbox" checked={todo.finished} onClick={() => todo.finished = !todo.finished}/>
-        {todo.title}
-    </li>
-});
-@observer
-class TodoListView extends Component {
-    render() {
-        return <div>
-            <ul>
-                {this.props.todoList.todos.map((todo) =>
-                    <TodoView todo={todo} key={todo.id}/>)}
-            </ul>
-            Tasks left:{this.props.todoList.unfinishedTodoCount}
-        </div>;
-    }
-}
-
-const store = new TodoList();
-
-ReactDOM.render(<TodoListView todoList={store}/>, document.getElementById('root'));
-
-autorun(()=>{
-    console.log("Tasks left: " + store.unfinishedTodoCount);
-});
-store.todos.push(
-    new Todo("Get Coffee"),
-    new Todo("Write simpler code")
-);
-store.todos[0].finished = true;
