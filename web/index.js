fcf33a05e791415e471a1f4a6a46bf953daca236
nalantianyi <nalantianyi@icloud.com>
2017-09-08 16:30:22 +0800

    //

--- web/index.js
+++ web/index.js
@@ -1,3 +1,55 @@
-console.log(123);
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {observer} from 'mobx-react';
+import {observable, computed,autorun} from 'mobx';
 
 
+class Todo {
+    id = Math.random();
+    @observable title = '';
+    @observable finished = false;
+    constructor(title){
+        this.title=title;
+    }
+}
+
+class TodoList {
+    @observable todos = [];
+
+    @computed
+    get unfinishedTodoCount() {
+        return this.todos.filter((todo) => !todo.finished).length;
+    }
+}
+
+const TodoView = observer(({todo}) => {
+    return <li>
+        <input type="checkbox" checked={todo.finished} onClick={() => todo.finished = !todo.finished}/>
+        {todo.title}
+    </li>
+});
+@observer
+class TodoListView extends Component {
+    render() {
+        return <div>
+            <ul>
+                {this.props.todoList.todos.map((todo) =>
+                    <TodoView todo={todo} key={todo.id}/>)}
+            </ul>
+            Tasks left:{this.props.todoList.unfinishedTodoCount}
+        </div>;
+    }
+}
+
+const store = new TodoList();
+
+ReactDOM.render(<TodoListView todoList={store}/>, document.getElementById('root'));
+
+autorun(()=>{
+    console.log("Tasks left: " + store.unfinishedTodoCount);
+});
+store.todos.push(
+    new Todo("Get Coffee"),
+    new Todo("Write simpler code")
+);
+store.todos[0].finished = true;
